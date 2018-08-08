048cd6a0cd36a13d68805d4bca9152c34934e757
hsin78318 <hsin78318@gmail.com>
2018-08-08 13:44:42 +0800

    初始畫面、登入畫面（簡易UI)

--- Ropa/Controller /SignIn /FirstScreenViewController.swift
+++ Ropa/Controller /SignIn /FirstScreenViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class FirstScreenViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        signUpButton.layer.borderWidth = 1
+        signUpButton.layer.borderColor = UIColor.gray.cgColor
+        
         
     }
 
